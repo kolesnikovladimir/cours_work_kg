9de3891f9f3f69269b7b1157a87ccbd37744c898
kolesnikov <kolesnikov>
2022-12-22 17:56:46 +0300

    done

--- cg_course_v9/main.cpp
+++ cg_course_v9/main.cpp
@@ -1,19 +1,10 @@
-#include "../libwinbgi/src/graphics.h"
+
 #include <stdio.h>
-
-#include <iostream>
 #include "controller.h"
-
-#include "Dot.h"
-#include "mPolygon.h"
-#include "prisma.h"
 
 int main()
 {
-
-	Controller* scene = new Controller;
-
-	scene->create_data_call_menu();
-
+	Controller* data = new Controller;
+	data->create_data_call_menu();
 	return 0;
 }
